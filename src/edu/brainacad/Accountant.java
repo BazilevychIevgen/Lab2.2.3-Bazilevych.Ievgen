3ed0e7a1e74da221ef6bfdf448052c189afba4cc
Admin <baro@ikrt>
2016-02-22 15:47:38 +0200

    Lab2.2.3

---
+++ src/edu/brainacad/Accountant.java
@@ -0,0 +1,18 @@
+package edu.brainacad;
+
+/**
+ * Created by Admin on 22.02.2016.
+ */
+public class Accountant {
+
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee();
+        emp1.setNameEmployee("Jonny");
+
+        double sum0 =emp1.varargsSalary(154.5 ,147.5 , 1547.52);
+
+
+    }
+}
